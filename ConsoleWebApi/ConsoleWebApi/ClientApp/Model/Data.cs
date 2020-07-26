b3a40c398da1b8778ce1a26615abaad9b1da5353
riteshraj96 <42927463+riteshraj96@users.noreply.github.com>
2020-07-27 02:55:39 +0530

    Add files via upload
    The first line of the data file is getting skipped while printing out the values.

---
+++ ConsoleWebApi/ConsoleWebApi/ClientApp/Model/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.Model
+{
+    public class Data
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string EmailId { get; set; }
+        public string Gender { get; set; }
+        public DateTime Dob { get; set; }
+
+        public Data(String firstname, string secondname, string emailid, string gender, DateTime dob)
+        {
+            this.FirstName = firstname;
+            this.SecondName = secondname;
+            this.EmailId = emailid;
+            this.Gender = gender;
+            this.Dob = dob;
+        }
+    }
+}
